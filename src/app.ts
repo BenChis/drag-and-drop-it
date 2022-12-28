4beca8112ca978a9cbab8e00defa928039ebdf18
Benjamin Scharf <ben.project.it@gmail.com>
2022-12-28 14:43:46 +0000

    add autobind fn

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,24 @@
+// autobind decorator
+function autobind(_: any, _2: string, descriptor: PropertyDescriptor) {
+  const originalMethod = descriptor.value;
+  const adjDescriptor: PropertyDescriptor = {
+    configurable: true,
+    get() {
+      const boundFn = originalMethod.bind(this);
+      return boundFn;
+    },
+  };
+  return adjDescriptor;
+}
+
+// ProjectInput Class
 class ProjectInput {
   templateElement: HTMLTemplateElement;
   hostElement: HTMLDivElement;
   element: HTMLFormElement;
+  $titleInput: HTMLInputElement;
+  $descriptionInput: HTMLInputElement;
+  $peopleInput: HTMLInputElement;
 
   constructor() {
     this.templateElement = document.getElementById(
@@ -14,7 +31,46 @@ class ProjectInput {
       true
     );
     this.element = importedNode.firstElementChild as HTMLFormElement;
+    this.element.id = 'user-input';
+
+    this.$titleInput = this.element.querySelector('#title') as HTMLInputElement;
+    this.$descriptionInput = this.element.querySelector(
+      '#description'
+    ) as HTMLInputElement;
+    this.$peopleInput = this.element.querySelector(
+      '#people'
+    ) as HTMLInputElement;
+
+    this.configure();
     this.attach();
+  }
+
+  private gatherUserInput(): [string, string, number] | void {
+    const enteredTitle = this.$titleInput.value;
+    const enteredDescription = this.$descriptionInput.value;
+    const enteredPeople = this.$peopleInput.value;
+
+    if (
+      enteredTitle.trim().length === 0 ||
+      enteredDescription.trim().length === 0 ||
+      enteredPeople.trim().length === 0
+    ) {
+      alert('Invalid input, please try again');
+      return;
+    } else {
+      return [enteredTitle, enteredDescription, +enteredPeople];
+    }
+  }
+
+  @autobind
+  private submitHandler(event: Event) {
+    event.preventDefault();
+    const userInput = this.gatherUserInput();
+    console.log(userInput);
+  }
+
+  private configure() {
+    this.element.addEventListener('submit', this.submitHandler);
   }
 
   private attach() {
