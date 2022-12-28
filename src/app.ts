4c50c67ff410b913497eada9d7ca822337462387
Benjamin Scharf <ben.project.it@gmail.com>
2022-12-28 15:40:33 +0000

    create reusable validation

--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,47 @@
+// Validation
+interface Validatable {
+  value: string | number;
+  required?: boolean; // || undefined
+  minLength?: number;
+  maxLength?: number;
+  min?: number;
+  max?: number;
+}
+
+function validate(validatableInput: Validatable) {
+  let isValid = true;
+  if (validatableInput.required) {
+    isValid = isValid && validatableInput.value.toString().trim().length !== 0;
+  }
+  if (
+    validatableInput.minLength != null &&
+    typeof validatableInput.value === 'string'
+  ) {
+    isValid =
+      isValid && validatableInput.value.length >= validatableInput.minLength;
+  }
+  if (
+    validatableInput.maxLength != null &&
+    typeof validatableInput.value === 'string'
+  ) {
+    isValid =
+      isValid && validatableInput.value.length <= validatableInput.maxLength;
+  }
+  if (
+    validatableInput.min != null &&
+    typeof validatableInput.value == 'number'
+  ) {
+    isValid = isValid && validatableInput.value >= validatableInput.min;
+  }
+  if (
+    validatableInput.max != null &&
+    typeof validatableInput.value == 'number'
+  ) {
+    isValid = isValid && validatableInput.value <= validatableInput.max;
+  }
+  return isValid;
+}
+
 // autobind decorator
 function autobind(_: any, _2: string, descriptor: PropertyDescriptor) {
   const originalMethod = descriptor.value;
@@ -50,10 +94,29 @@ class ProjectInput {
     const enteredDescription = this.$descriptionInput.value;
     const enteredPeople = this.$peopleInput.value;
 
+    const titleValidatabel: Validatable = {
+      value: enteredTitle,
+      required: true,
+    };
+    const descriptionValidatabel: Validatable = {
+      value: enteredDescription,
+      required: true,
+      minLength: 5,
+    };
+    const peopleValidatabel: Validatable = {
+      value: +enteredPeople,
+      required: true,
+      min: 1,
+      max: 5,
+    };
+
     if (
-      enteredTitle.trim().length === 0 ||
-      enteredDescription.trim().length === 0 ||
-      enteredPeople.trim().length === 0
+      !validate(titleValidatabel) ||
+      !validate(descriptionValidatabel) ||
+      !validate(peopleValidatabel)
+      //   enteredTitle.trim().length === 0 ||
+      //   enteredDescription.trim().length === 0 ||
+      //   enteredPeople.trim().length === 0
     ) {
       alert('Invalid input, please try again');
       return;
